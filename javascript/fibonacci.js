132c4838e8cb3fb3fc80be15f619627ad633332c
Andrew Wilson <adwilson12901@gmail.com>
2024-05-21 20:28:38 -0400

    done

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,7 +1,16 @@
 function fibonacci(num) {
   // type your code here
-}
+  let first = 0
+  let second = 1
+  let sum = 0
 
+  for (let i = 0; i < num - 1; i++) {
+    sum = first + second
+    first = second
+    second = sum
+  }
+  return sum
+}
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
